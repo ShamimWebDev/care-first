26d73c02abb031557e0497a864dd8d27e75dd457
shamimwebdev <shamim.ct17@gmail.com>
2025-12-24 08:56:44 +0600

    deploy to vercel

--- app/api/create-checkout-session/route.js
+++ app/api/create-checkout-session/route.js
@@ -3,6 +3,8 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request) {
   try {
     const body = await request.json();
